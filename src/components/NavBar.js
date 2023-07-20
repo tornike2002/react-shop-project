71ce53e0c86d13dbbb7b78ae27db1a8ec0e58899
Tornike2002 <butiashvili.t@gtu.ge>
2023-07-20 20:31:50 +0400

    created shop project

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <Fragment>
+      <nav>
+        
+      </nav>
+    </Fragment>
+  );
+};
+
+export default NavBar;
